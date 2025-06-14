94490b3b30082f7f6b3cd2f604801e947b94b9a1
desmondlsl <68117020+desmondlsl@users.noreply.github.com>
2025-06-14 18:44:14 +0800

    Update crawl_utils.py

--- crawling/crawl_utils.py
+++ crawling/crawl_utils.py
@@ -22,8 +22,8 @@ async def emitRequest(url: str, client: httpx.AsyncClient, headers={}):
         await asyncio.sleep(retry_timeout)
         retry_timeout = min(retry_timeout * 2, RETRY_TIMEOUT_MAX)
       else:
-        r.raise_for_status()
         print("Got response: " + r.headers + r.text)
+        r.raise_for_status()
         raise Exception(r.status_code, url)
     except (httpx.PoolTimeout, httpx.ReadTimeout, httpx.ReadError) as e:
       logger.warning(
